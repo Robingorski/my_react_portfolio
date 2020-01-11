2cd90a1995f7e1398e70cc5568ac731f7f21f84f
Robingorski <robin.gorski@student.kyh.se>
2020-01-11 13:13:36 +0100

    added small landingpage and menu options

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hamburger = () => {
-  return <div className=''>menu</div>;
+  return <div className='hamburger-menu'>
+    <div className="menu-secondary-backround-color"></div>
+    <div className="menu-layer">
+      <div className="menu-city-background"></div>
+      <div className="container">
+        <div className="wrapper">
+          <div className="menu-links">
+            <nav>
+              <ul>
+                <li>
+                  <Link to='/opportunities'>Opportunities</Link>
+                </li>
+                <li>
+                  <Link to='/solutions'>Solutions</Link>
+                </li>
+                <li>
+                  <Link to='/contact-us'>Opportunities</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="info">
+              <h3>Our Promise</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </div>
+            <div className="locations">
+              Locations:
+              <span>Projekt 1</span>
+              <span>Projekt 2</span>
+              <span>Projekt 3</span>
+              <span>Projekt 4</span>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Hamburger;
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Hamburger from './Hamburger'
 
 const Header = () => {
-  return <header></header>;
+  return <header>
+    <div className="container">
+      <div className="wrapper">
+        <div className="inner-header">
+          <div className="logo">
+            <Link to="/"> HAMBRG.</Link>
+          </div>
+          <div className="menu">
+            <button>
+              Menu
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Hamburger />
+  </header>;
 };
 
 export default Header;
